9123e5dd35b10fc92b0dfa1ad98d719edb4b2822
Nithin0620 <nithin30302@gmail.com>
2025-06-28 22:20:46 +0530

    .

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -29,11 +29,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"));
 });
 
 
